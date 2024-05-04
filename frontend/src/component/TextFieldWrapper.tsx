c508b2dc04956abef70a9538b1ef7b05d5bf1cac
TodayNightt <sd.teoh.tsd@gmail.com>
2024-05-04 16:59:28 +0800

    frontend) rename TextFieldWrapper -> TextWrapper and added the option to change to textarea

--- frontend/src/component/TextFieldWrapper.tsx
+++ frontend/src/component/TextFieldWrapper.tsx
@@ -1,14 +1,16 @@
 import { TextField } from "@kobalte/core";
-import { createMemo } from "solid-js";
+import { Match, Switch, createMemo } from "solid-js";
 
 type TextFieldWithGridProps = {
 	defaultValue?: string;
 	name: string;
 	label: string;
 	disabled: boolean;
+	type: "textfield" | "textarea";
+	tailwind?: string;
 };
 
-export default function TextFieldWrapper(props: TextFieldWithGridProps) {
+export default function TextWrapper(props: TextFieldWithGridProps) {
 	const defaultVal = createMemo(() => props.defaultValue);
 	return (
 		<TextField.Root
@@ -16,16 +18,27 @@ export default function TextFieldWrapper(props: TextFieldWithGridProps) {
 			name={props.name}
 			class={`flex items-center gap-4 flex-wrap ${
 				props.disabled ? "pointer-events-none" : ""
-			}`}
+			} ${props.tailwind}`}
 		>
 			<TextField.Label class="flex label-style min-w-24 md:justify-end sm:justify-start">
 				{props.label}
 			</TextField.Label>
-			<TextField.Input
-				value={defaultVal()}
-				placeholder={props.name}
-				class={"rounded-xl px-4 py-2 font-medium"}
-			/>
+			<Switch>
+				<Match when={props.type === "textfield"}>
+					<TextField.Input
+						value={defaultVal()}
+						placeholder={props.name}
+						class={"rounded-xl px-4 py-2 font-medium"}
+					/>
+				</Match>
+				<Match when={props.type === "textarea"}>
+					<TextField.TextArea
+						value={defaultVal()}
+						placeholder={props.name}
+						class="rounded-xl px-4 py-2 font-medium flex-grow h-4/5"	
+					/>
+				</Match>
+			</Switch>
 		</TextField.Root>
 	);
 }
