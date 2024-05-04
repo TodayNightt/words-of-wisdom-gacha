824542bef4e48720678de42f3b82c5c0245a4434
TodayNightt <sd.teoh.tsd@gmail.com>
2024-05-04 23:46:42 +0800

    frontend) moved environment typecheck to app
    mentioned in fe1199bf0e321c98f6a7395ad817668677678ce6, it didn't work as
    promised

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -4,8 +4,10 @@ import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
 import { MetaProvider } from "@solidjs/meta";
+import { Environment } from "./environments";
 
 export default function App() {
+	Environment.getInstance();
 	return (
 		<Router
 			root={(props) => (
